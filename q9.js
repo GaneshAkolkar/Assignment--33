ba34dfb5c33a454f41a318a337a563e27fc0ba77
Ganesh Akolkar <112386641+GaneshAkolkar@users.noreply.github.com>
2023-09-16 15:32:38 +0530

    Add files via upload

---
+++ q9.js
@@ -0,0 +1,5 @@
+function search() {
+    var searchInput = document.getElementById("searchInput").value;
+    // Implement your search functionality here
+    // You can redirect the user to a search results page or perform other actions.
+}
